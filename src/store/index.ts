ce3aff98efde5dc05ef4b967fa495515c6c7e831
Raman Zherdzeu <raman_zherdzeu@epam.com>
2020-10-28 10:15:38 +0300

    Remove logging for prod

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,11 +4,13 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { AppState, appReducer } from './app-reducer';
 import logger from './middlewares/logger';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default function configureStore(preloadedState?: AppState) {
     const middlewares = [logger];
     const middlewareEnhancer = applyMiddleware(...middlewares);
 
-    const enhancers = [middlewareEnhancer];
+    const enhancers = isProduction ? [] : [middlewareEnhancer];
     const composedEnhancers = composeWithDevTools(...enhancers);
 
     return createStore(appReducer, preloadedState, composedEnhancers);
